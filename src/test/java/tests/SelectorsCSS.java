d389d6f784f2086d61e2e9227b2e88e7f2037450
Alina-QA-Engineer <alinaqatests@gmail.com>
2024-11-19 22:10:15 +0200

    Second Commit

--- src/test/java/tests/SelectorsCSS.java
+++ src/test/java/tests/SelectorsCSS.java
@@ -1,4 +1,60 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class SelectorsCSS {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void elementsHeaderIlCarro() {
+
+        driver.navigate().to("https://ilcarro.web.app/search");
+
+        threadSleep();
+        WebElement btnLetTheCarWorkHeader = driver.findElement(By.cssSelector("a[id='1']"));
+        btnLetTheCarWorkHeader.click(); // Click on the "Let the car work" link in the header
+        System.out.println("1. " + btnLetTheCarWorkHeader.getText()); // Text output
+
+        threadSleep();
+        WebElement btnSearchHeader = driver.findElement(By.cssSelector("a[id='0']"));
+        btnSearchHeader.click(); // Click on the "Search" link in the header
+        System.out.println("2. " + btnSearchHeader.getText());
+
+        threadSleep();
+        WebElement btnTermsOfUseHeader = driver.findElement(By.cssSelector("a[id='2']"));
+        btnTermsOfUseHeader.click(); // Click on the "Terms of use" link in the header
+        System.out.println("3. " + btnTermsOfUseHeader.getText());
+
+        threadSleep();
+        WebElement btnSignUpHeader = driver.findElement(By.cssSelector("a[ng-reflect-router-link='registration']"));
+        btnSignUpHeader.click(); // Click on the "Registration" link in the header
+        System.out.println("4. " + btnSignUpHeader.getText());
+
+        threadSleep();
+        WebElement btnLogInHeader = driver.findElement(By.cssSelector("a[ng-reflect-router-link='login']"));
+        btnLogInHeader.click(); // Click on the "Registration" link in the header
+        System.out.println("5. " + btnLogInHeader.getText());
+
+        threadSleep();
+        WebElement btnLogoHeader = driver.findElement(By.cssSelector("div.header img[alt='logo']"));
+        btnLogoHeader.click(); // Click on the logo in the header
+
+        threadSleep();
+        driver.quit();
+
+    }
+
+    public static void threadSleep() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/test/java/tests/StartTests.java
+++ src/test/java/tests/StartTests.java
@@ -1,4 +1,50 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class StartTests {
+
+    WebDriver driver = new ChromeDriver();
+    //  System.setProperty("webdriver.chrome.driver", "/Users/alinakolestinskaa/Tools/chromedriver-mac-arm64");
+
+    @Test
+    public void firstTest() {
+
+        driver.navigate().to("https://telranedu.web.app/home"); // Открывает страницу в браузере
+
+        try {
+            Thread.sleep(1000); // sleep - Останавливает выполнение программы
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Указываем локатор кнопки "Login" для ее нахождения и клику по ней
+        WebElement btnLoginHeader = driver.findElement(By.cssSelector("a[href='/login']"));
+        btnLoginHeader.click();
+
+        WebElement btnRegistration = driver.findElement(By.cssSelector("button[name='registration']"));
+        btnRegistration.click();
+
+        // driver.quit(); // Закрывает браузер
+    }
+
+    @Test
+    public void secondTest() {
+
+        driver.navigate().to("https://telranedu.web.app/home");
+        WebElement elementTextH2 = driver.findElement(By.cssSelector("h2"));
+        System.out.println(elementTextH2.getText());
+
+        WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+        System.out.println("Tag Name > " + btnAbout.getTagName()); // Указывает имя тега
+        System.out.println("Text > " + btnAbout.getText()); // Показывает текст
+
+        driver.quit();
+
+    }
+
 }
